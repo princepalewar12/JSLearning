9c236cb6a4be05fc12838ed426750bcd5dc017ba
Prince_Palewar <princepalewar12@gmail.com>
2023-09-20 08:51:30 +0530

    variable assignment
    learned about variables and completed assignment on the same.

---
+++ 01_variable.js
@@ -0,0 +1,38 @@
+// var firstName = "Pince Palewar";
+// console.log(firstName);
+
+// function name(surname) {
+//     console.log("Prince ", surname);
+// }
+// name("palewar");
+var firstName; // variable declaration
+
+firstName = "Prince";  // Variable initialization
+
+console.log(firstName);
+
+firstName = "Mr. Prince"; // Variable update
+
+console.log(firstName);
+
+
+
+
+
+var lastName = "Palewar"; // variable declaration and initialization
+
+console.log(lastName);
+
+
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "6"; // initialization
+
+console.log(height);
+
+height = "6.2 feet";// update
+
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Prince Palewar";
+console.log(myFullName);
+myFullName="Prince Bharat Palewar";
+console.log(myFullName);
+
+var myLove="I LOVE ONLY JS";
+console.log(myLove);
+
+var firstName ="Prince";
+console.log("First Name: ",firstName)
+var lastName = "Palewar";
+console.log("Last Name: ",lastName);
+var age = "25";
+console.log("Age: ",age);
+var address = "Nagpur";
+console.log("Address: ",address);
+var pinCode = "440002";
+console.log("Pincod: ",pinCode);
+var gender = "Male";
+console.log("Gender: ", gender);
+var isMarried = "No I Am Not";
+console.log("Married: ",isMarried);
+
