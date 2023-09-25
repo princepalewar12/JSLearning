dad715a07aba58fca2f87561bbd3071008ea1ef6
Prince_Palewar <princepalewar12@gmail.com>
2023-09-25 16:15:47 +0530

    completed assignment related

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,14 @@
+function stringBasics(){
+    var company ="Adobe.";
+    var hobby1 = "Treveling";
+    var hobby2 = "Reading";
+    var hobby3 = "playing Guitar";
+    console.log("My dream company is ",company);
+    console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    var output = hobby1.length+hobby2.length+hobby3.length;
+    console.log("Length of Hobby1:",hobby1.length,".");
+    console.log("Length of Hobby2:",hobby2.length,".");
+    console.log("Length of Hobby3:",hobby3.length,".");
+    console.log("Total length of all the strings is",output);
+}
+stringBasics();
