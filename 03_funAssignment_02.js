12bef7c55e9742cb312004e24e03df17b001647d
Prince_Palewar <princepalewar12@gmail.com>
2023-09-25 15:40:39 +0530

    created part two of assignment 03
    assignment is related to function

---
+++ 03_funAssignment_02.js
@@ -0,0 +1,6 @@
+function bankDetail(bankName, accountNum, location, pinCode) {
+    console.log("Bank Name:",bankName,",","Account Number:",accountNum,",","Location:",location,",","Pincode:",pinCode);
+}
+bankDetail("CITI Bank",3456782345,"pune",431202);
+bankDetail("Axis Bank",7856782345,"Mumbai",441202)
+bankDetail("HDFC Bank",8956782345,"Pune",631202,"open")
