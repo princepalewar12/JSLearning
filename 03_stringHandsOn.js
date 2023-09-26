3fab52fe8051e40215d5e9e89bd2b60edd70656b
Prince_Palewar <princepalewar12@gmail.com>
2023-09-26 16:16:21 +0530

    string methods assignment

---
+++ 03_stringHandsOn.js
@@ -0,0 +1,23 @@
+function stringHandsOn() {
+    var givenstr = "   Hey you are doing good, keep it up    "
+    console.log(`1. Given string is: ${givenstr}`);
+    console.log(`2. The length of given string is :${givenstr.length}`);
+    var leadingSpace = givenstr.trim();
+    console.log(`3.1 The given string after removing extra spaces: ${leadingSpace}`);
+    console.log(`3.2 The length of given string after removing extra spaces is: ${givenstr.trim().length}`);
+    var countExtraSpace = givenstr.length-givenstr.trim().length;
+    console.log(`4. The number of extra spaces has been removed from the given string is:${countExtraSpace}`);
+    var firstAndLastChar = leadingSpace.charAt(0);
+    console.log(`5. The first character after removing extra spaces is "${firstAndLastChar}" and the last character is "${leadingSpace.charAt(leadingSpace.length-1)}"`);
+    var wordCount = leadingSpace.split(" ");
+    console.log(`6. Total number of words in the string after removing extra spaces are ${wordCount.length}.`);
+    console.log(`7. The index number of word "good" is ${leadingSpace.indexOf('good')}.`);
+    console.log(`8.1 Substring starting from index 22 by slice() method is "${leadingSpace.slice(22)}".`);
+    console.log(`8.2 Substring starting from index 22 by subsrting() method is "${leadingSpace.substring(22)}".`);
+    console.log(`9. check the given string is ending with "up" or not: ${leadingSpace.endsWith('up')}.`);
+    console.log(`9. check the given string is start with "Hey" or not: ${leadingSpace.startsWith('Hey')}.`);
+}   
+stringHandsOn()
+
+count : PRINCE PALEWAR 
+SHA KEY: 
---
+++ 04_stringBasics.js
@@ -0,0 +1,82 @@
+console.log("====================charAt()===============================");
+var greet = "Good Morning";
+var res = greet.length;
+console.log("Total number of character in greet is", res);
+// var charAt = greet.charAt(0);
+// console.log(charAt);
+console.log("Character at index 0 in greet is", greet.charAt(0));
+// var indexOfGreet = greet.length-1;
+console.log("The last third character in the greet is", greet.charAt(greet.length - 3));
+console.log("====================concate()===============================");
+var firstName = "Prince ";
+var lastName = "Palewar";
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
+console.log("========== concatenation =============== ");
+
+var result = firstName.concat(lastName);
+console.log(result);
+
+console.log("========== indexOf =============== ");
+
+var indexOfM = greet.lastIndexOf('M');
+console.log('Index of M is: ', indexOfM);
+console.log(greet.indexOf('nin'));
+console.log('Index of char z:', greet.indexOf('z'));
+console.log('Index of char D:', greet.indexOf('D'));
+
+console.log("========== replace =============== ");
+
+var result =greet.replace('Morning', "Afternoon");
+console.log(result);
+var result = greet.replace(' ', "-");
+console.log(result);
+
+console.log("========== trim() =============== ");
+var str = "   Good Afternoon  ";
+var lengthBeforeTrim = str.length;
+console.log("Length before trim : ", str.length);
+var result = str.trim();
+console.log("Length after trim: ", result.length);
+var lengthAfterTrim = result.length;
+var result = lengthBeforeTrim - lengthAfterTrim;
+console.log('Removed extra spaces are: ', result);
+
+console.log("========== includes() =============== ");
+var result = greet.includes('Morning');
+console.log("Is Morning word available: ", result);
+var result = greet.includes('nin');
+console.log("Is nin word available: ", result);
+var result = greet.includes('good');
+console.log("Is good word available: ", result);
+
+console.log("========== search() =============== ");
+var greet = "Good Morning";
+var result = greet.search('orn');
+console.log(result);
+var result = greet.search('after');
+console.log(result);
+
+console.log("========== slice() =============== ");
+var greet = 'Hello Good Morning!';
+var result = greet.slice(6);
+console.log(result);
+var result = greet.slice(6, 10);
+console.log(result);
+
+console.log("========== split() =============== ");
+var result = greet .split(" ");
+console.log(result);
+
+//WAP to count the number of words
+
+var str = "I am a UI developer";
+var split = str.split(" ");
+console.log("The number of words in the str variable is ",split.length);
+console.log(`Total words are ${split.length}`);
+console.log("data type:",typeof split);
+
+// String template
+console.log("I am a \"UI\" developer"); 
+console.log(`I am a "UI" developer`); 
