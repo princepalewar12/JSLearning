5f9ab5dacb1c06aa97adc3a20003c59e510f6cee
Prince_Palewar <princepalewar12@gmail.com>
2023-09-27 10:46:08 +0530

    operator Assignment part 2

---
+++ 05_operatorB.js
@@ -0,0 +1,19 @@
+
+console.log(`+++++++++++++++++++++++++++++++++++ TASK 01 ++++++++++++++++++++++++`);
+function greaterNumber(value1, value2){
+    console.log(`1. In between ${value1} and ${value2}`);
+    var result = value1>value2 ? console.log(`The greatest number is ${value1}`):""|| value2>value1 ? console.log(`The greatest number is ${value2}`):"";
+}
+greaterNumber(10,-10)
+greaterNumber(800,899)
+
+console.log(`+++++++++++++++++++++++++++++++++++ TASK 02 ++++++++++++++++++++++++`);
+function isEvenOrOddNum(num) {
+    var result = num%2==0 ? console.log(`2. The given number ${num} is even: True.`) : console.log(`2. The given number ${num} is odd: False`);
+}
+isEvenOrOddNum(69);
+console.log(`+++++++++++++++++++++++++++++++++++ TASK 03 ++++++++++++++++++++++++`);
+function wordLength(word) {
+    var result = word.length%2==0 ? console.log(`3. The length of given word "${word}" is EVEN.`) : console.log(`3. The length of given word "${word}" is ODD.`);;
+}
+wordLength("CodeMindTechnology");
