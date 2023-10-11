eebe79edc9b5466c978068b5d1d8d48167347f99
Prince_Palewar <princepalewar12@gmail.com>
2023-10-11 14:33:44 +0530

    learned about function constructor to create an object from it.

---
+++ 06_factorial.js
@@ -0,0 +1,19 @@
+function factorial(num){
+    let factNum = 1;
+    for(let index = num; index>=1; index --){
+        factNum = factNum * index;
+    }
+    console.log(`Factorial of ${num} is ${factNum}`);
+}
+factorial(5);
+factorial(7);
+factorial(10);
+
+
+console.log(`================= Prime Number ================`);
+function isPrime(){
+    for (let index = ; index < rray.length; index++){
+        const element = array[index];
+       
+    }
+}
---
+++ 09_date.js
@@ -0,0 +1,8 @@
+const date = new Date();
+console.log(date.toTimeString());
+
+const square10 = Math.pow(10);
+console.log(square10);
+
+const maxNum=Math.max(23.56, 67, 89, 90.567, 45, 67);
+console.log(maxNum);
